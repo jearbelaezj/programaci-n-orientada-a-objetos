9c85385883efb3b74d096d601576ebdb621a9b60
esteb <jearbelaezj@ut.edu.co>
2025-05-03 12:21:09 -0500

    joda funciona

--- Clases/Animal.java
+++ Clases/Animal.java
@@ -1,4 +1,14 @@
 package Clases;
 
 public class Animal {
+    public String nombre;
+    public String tipo;
+    public Animal(String nombre,String tipo){
+        this.nombre = nombre;
+        this.tipo = tipo;
+    }
+    public void comer(){
+        System.out.println("Comienza comida");
+    }
+
 }
---
+++ Clases/Main.java
@@ -0,0 +1,12 @@
+package Clases;
+
+public class Main {
+    public static void main(String[] args) {
+        Animal perro = new Animal("mylon","perro");
+        Animal gato = new Animal("gato", "malparido");
+        perro.comer();
+        gato.comer();
+
+
+    }
+}
