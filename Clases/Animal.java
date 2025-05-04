0c30e458652e51272856d988d3df01617597295f
ANICHIARICO <aeanichiaricov@ut.edu.co>
2025-05-04 16:58:52 -0500

    modificacion 1

--- Clases/Animal.java
+++ Clases/Animal.java
@@ -1,4 +1,17 @@
 package Clases;
 
 public class Animal {
+    public String nombre;
+    public String tipo;
+    public Animal(String nombre,String tipo){
+        this.nombre = nombre;
+        this.tipo = tipo;
+    }
+    public void comer(){
+        System.out.println("Comienza comida");
+    }
+    public void morder(){
+        System.out.println("me ha mordido este hijuputa");
+    }
+
 }
