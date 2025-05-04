826c02f812d846a3ae0d25df1654f4456c7f81e0
esteb <jearbelaezj@ut.edu.co>
2025-05-04 17:22:30 -0500

    modificacion como 4002399322

--- Clases/Animal.java
+++ Clases/Animal.java
@@ -3,7 +3,7 @@ package Clases;
 public class Animal {
     public String nome;
     public String raca;
-    public jirafa(String nome, String raca){
+    public Animal(String nome, String raca){
         this.nome = nome;
         this.raca = raca;
     }
--- Clases/Main.java
+++ Clases/Main.java
@@ -1,4 +1,7 @@
 package Clases;
 
 public class Main {
+    public static void main(String[] args) {
+        Animal perro= new Animal("","");
+    }
 }
