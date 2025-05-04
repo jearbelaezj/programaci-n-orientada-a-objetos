72e2985daae88a31ca88a08960fde72e96e12dce
ANICHIARICO <aeanichiaricov@ut.edu.co>
2025-05-04 16:54:46 -0500

    modificacion 1

--- Clases/Animal.java
+++ Clases/Animal.java
@@ -1,4 +1,10 @@
 package Clases;
 
 public class Animal {
+    public String nombre;
+    public String tipo;
+    public Animal(String nombre, String tipo) {
+        this.nombre = nombre;
+        this.tipo = tipo;
+    }
 }
---
+++ Clases/main.java
@@ -0,0 +1,8 @@
+package Clases;
+
+public class main {
+    public static void main(String[] args) {
+        Animal perro = new Animal("perro", "perro");
+        Animal gato = new Animal("gato", "gato");
+    }
+}
