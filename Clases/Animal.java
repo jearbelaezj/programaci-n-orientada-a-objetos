ea184568360b8e1d16b8af599822de04d530c20d
esteb <jearbelaezj@ut.edu.co>
2025-05-04 17:21:01 -0500

    modificacion como 40

--- Clases/Animal.java
+++ Clases/Animal.java
@@ -1,4 +1,10 @@
 package Clases;
 
 public class Animal {
+    public String nome;
+    public String raca;
+    public jirafa(String nome, String raca){
+        this.nome = nome;
+        this.raca = raca;
+    }
 }
